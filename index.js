50f2a18e144fd4b80767d74351c1aa97870bb08a
ShubHH03 <mak.shubham2003@gmail.com>
2026-04-28 16:25:53 +0530

    refactor: improve server setup and add health check endpoint; create initial HTML file

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 import http from 'node:http';
-import express from 'express'
-import { Socket } from 'socket.io';
+import express from 'express';
+import { Server } from 'socket.io';
+import path from 'node:path';
 
-async function main(){
+async function main() {
+	const PORT = process.env.PORT ?? 8000;
 
-	const PORT = process.env.PORT ?? 8000
+	const app = express();
+	const server = http.createServer(app);
+	const io = new Server();
 
-	const app = express() 
-	const server = http.createServer(app)
-	const io = new Server()
+  
+	io.attach(server);
 
-	io.attach(server)
-
+  app.use(express.static(path.resolve('./public')))
+  
+	app.get('/health', (req, res) => {
+		return res.json({ message: 'all good' });
+	});
 
 	server.listen(PORT, () => {
 		console.log(`Server running on PORT: ${PORT}`);
-	})
+	});
 }
+
+main();
